363f3cbc719c0681723a3a39751108f278aeeb93
mreichardt95 <mreichardt95@gmail.com>
2026-03-23 16:07:26 +0100

    update ghostty config

--- modules/home/desktop/terminals.nix
+++ modules/home/desktop/terminals.nix
@@ -21,8 +21,18 @@ _:
           kitty
         ];
 
-      xdg.configFile."ghostty" = {
-        source = create_symlink "${dotfiles}/config/ghostty/";
+      xdg.configFile."ghostty/config".text = ''
+        command = ${pkgs.tmux}/bin/tmux new-session -A -s main
+        theme = Catppuccin Macchiato
+        background-opacity=1.0
+        window-theme=dark
+        font-thicken = true
+        macos-titlebar-style = native
+        keybind = cmd+grave_accent=toggle_quick_terminal
+      '';
+
+      xdg.configFile."ghostty/themes" = {
+        source = create_symlink "${dotfiles}/config/ghostty/themes/";
         recursive = true;
       };
 
